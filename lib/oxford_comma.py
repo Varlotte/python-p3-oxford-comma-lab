bd902ac3b748e0c004966d912f3d05673f37d42b
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-09-16 20:19:43 -0400

    passes 4 tests

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) >= 3:
+        comma_items = items[0:-1]
+        return ', '.join(comma_items) + ', and ' + items[-1]
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else:
+        return items[0]
